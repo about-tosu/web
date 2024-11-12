3f2f4856682b6a7ee9094cdb56e10823c3fec552
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2024-11-13 00:57:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 query = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 ydl_opts = {
-    'cookies': 'path_to_cookies.txt',  # Use the path to your cookies file
+    'cookies': 'cookies/cookies.txt',  # Use the path to your cookies file
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
